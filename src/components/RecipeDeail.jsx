292ed1c5edd10acc73ef429b9a4ad021a7256979
AliAais <aaisali228@gmail.com>
2024-02-16 02:43:02 -0800

    chaned looks and refactored code

--- src/components/RecipeDeail.jsx
+++ src/components/RecipeDeail.jsx
@@ -11,34 +11,43 @@ const RecipeDetail = () => {
 
         document.title="Recipe Detail";
 
-        const fetchRecipe=async()=>{
-                setLoading(true)
+        const fetchRecipe = async () => {
+                setLoading(true);
                 try {
-                const resp=await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-                const data=await resp.json()
-                setRecipe(data.meals[0])
-                setLoading(false)
+                    const resp = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+                    const data = await resp.json();
+                    const fetchedRecipe = data.meals[0];
+            
+                    if (fetchedRecipe && fetchedRecipe.strInstructions) {
+                        // Format strInstructions and update the state
+                        const formattedInstructions = fetchedRecipe.strInstructions.replace(/\n/g, '<br>');
+                        setRecipe(prevRecipe => ({ ...prevRecipe, strInstructions: formattedInstructions }));
+                    }
+            
+                    setLoading(false);
                 } catch (error) {
-                console.error(error)
+                    console.error(error);
                 }
-        }
-
-        React.useEffect( ()=>{
-                fetchRecipe()
-        },[])
+            };
+            
+            React.useEffect(() => {
+                fetchRecipe();
+            }, [id]); // Ensure that the effect runs when 'id' changes
+            
+        
     return (
         <>
         {
                 loading ? (<div className="flex items-center bg-slate-800 justify-center h-screen"><Loader/></div> ) :
         
         <div className='bg-slate-800 min-h-screen'>
-                <div className='w-[80vw] mx-auto '>
-                <div className='flex-col flex md:flex-row justify-between mb-4 md:mb-0 p-4'>
+                <div className='px-3 md:px-4 md:w-[90%] mx-auto'>
+                <div className='md:w-[90%] mx-auto flex-col flex md:flex-row justify-between mb-4 md:mb-0 py-4'>
                 <div className='text-white'>
-                <h1 className='text-4xl underline underline-offset-1 hover:underline-offset-4 duration-150 my-4'>{recipe.strMeal}</h1>
-                <h1 className='text-md my-2'>Area : {recipe.strArea}</h1>
-                <h1 className='text-md my-2 text-gray-500 underline'>Category : {recipe.strCategory}
-                <ol className='flex flex-wrap gap-4 mt-2 text-white'>
+                <h1 className='text-3xl font-bold md:text-4xl  my-4 '>{recipe.strMeal}</h1>
+                <h1 className='text-md my-2'>Dish Origin : {recipe.strArea}</h1>
+                <h1 className='text-sm md:text-md my-2 text-gray-500 underline'>Category : {recipe.strCategory}
+                <ol className='flex flex-wrap gap-4 mt-2 text-white text-xs'>
                 <li>{recipe.strIngredient1}</li>
                 <li>{recipe.strIngredient2}</li>
                 <li>{recipe.strIngredient3}</li>
@@ -55,14 +64,16 @@ const RecipeDetail = () => {
                 <li>{recipe.strIngredient14}</li>
                 </ol>
                 </h1>
-                <a href={recipe.strYoutube} className='text-red-500 font-bold text-2xl underline underline-offset-1 hover:underline-offset-4 duration-150'>Watch on Youtube</a>
+                <a href={recipe.strYoutube} className='text-red-500 font-bold text-md md:text-2xl underline underline-offset-1 hover:underline-offset-4 duration-150'>Watch on Youtube</a>
                 </div>
                 <img src={recipe.strMealThumb} alt={recipe.strMeal}
-                className='w-[100%] h-[100%] md:w-[250px] md:h-[250px] mt-5 md:mt-0' />
+                className='w-[80%] h-[80%] md:w-[250px] md:h-[250px] mx-auto mt-6 md:mt-0' />
                 </div>
-                <div className='w-[80vw] mx-auto p-4'>
-                <h1 className='text-2xl text-white mb-4 underline'>Instructions</h1>
-                <p className='text-white'>{recipe.strInstructions}</p>
+                <div className='md:w-[80vw] mx-auto '>
+                <h1 className='text-2xl text-white 
+                 border-t pt-4'>Instructions :</h1>
+                <p className='text-white text-sm' dangerouslySetInnerHTML={{ __html: recipe.strInstructions }}></p>
+
                 </div>
                 </div>
         </div>
